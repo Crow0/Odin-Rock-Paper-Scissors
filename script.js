d6621e5da0a6579d354e7a6a00e5fe10ab3e203d
Crow0 <75340423+Crow@users.noreply.github.com>
2024-02-19 20:55:05 +0100

    Add function for player choice

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function getComputerChoice() {
     const choices = ["rock","paper","scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose between rock,paper or scissors: ").toLowerCase();
+    if (playerChoice !=="rock" && playerChoice !=="paper" && playerChoice !=="scissors") {
+        alert("That choice is not valid. Please choose rock,paper or scissors: ")
+        return null;
+    }
+    return playerChoice;
+}
