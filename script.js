d726d1ffb4ccac8d3bd5625abcdcc1276e4a11a2
Crow0 <75340423+Crow@users.noreply.github.com>
2024-02-25 18:59:43 +0100

    Clean up the code

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-function getPlayerChoice(choice) {
-    let playerChoice = choice;
-    //prompt("Choose between rock,paper or scissors: ").toLowerCase();
-    if (playerChoice !=="rock" && playerChoice !=="paper" && playerChoice !=="scissors") {
-        alert("That choice is not valid. Please choose rock,paper or scissors: ")
-        return null;
-    }
-    return playerChoice;
-}
 
 function round(playerSelection, computerSelection) {
     if(playerSelection === "rock" && computerSelection === "scissors") {
@@ -37,35 +28,6 @@ function round(playerSelection, computerSelection) {
     }
 }
 
-/*
-function game () {
-    let PlayerScore = 0;
-    let ComputerScore = 0;
-
-    for(let i = 0; i<5; i++) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-
-        let result = round(playerSelection, computerSelection);
-        alert(result);
-        if (result === "Great job you won! Rock beats scissors." || result === "Great job you won! Paper beats rock." || result === "Great job you won! Scissors beat paper. ") {
-            PlayerScore++
-        } else if (result === "Sorry you lost. Try again. ") {
-            ComputerScore++
-        }
-    }
-
-    if (PlayerScore>ComputerScore ) {
-        alert("Congratulations, you win the game!");
-    } else if(PlayerScore<ComputerScore) {
-        alert("Sorry, you lost the game.");
-    } else {
-        alert("The game is a tie! ");
-    }
-}
-
-game();
-*/
 function playGame(choice) {
     let computerPick = getComputerChoice();
     result = round(choice,computerPick);
@@ -97,12 +59,6 @@ function playGame(choice) {
 }
 
 
-
-
-
-
-
-//let selection;
 let playerScore = 0;
 let computerScore = 0;
 
@@ -124,21 +80,7 @@ scissorsSelector.addEventListener("click",()=>playGame("scissors"));
 
 
 
-//paperSelector.addEventListener("click",getPlayerChoice("paper"));
-//scissorsSelector.addEventListener("click",getPlayerChoice("scissors"));
 
-
-
-
-
-
-
-
-/*
-let next = getPlayerChoice("rock");
-
-console.log(next);
-*/
 
 
 
